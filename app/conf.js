40138524dc21ed2c71d165655b40a1cb6bfae5d5
Knight, Eddie <iv.eddieknight@gmail.com>
2020-06-10 18:04:46 -0400

    Prod

--- app/conf.js
+++ app/conf.js
@@ -1,6 +1,6 @@
 module.exports = {
     sentryDsn: 'https://d0fca149b2114d8ea7378217a8fa5eb4@sentry.io/5191093',
-    isProduction: false, // Change this to true when building image! (This process is due for improvements)
+    isProduction: true, // Change this to true when building image! (This process is due for improvements)
     dataType: 'tsv',
     port: 80,
     logLevel: 'verbose',

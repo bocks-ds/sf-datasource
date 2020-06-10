7dbcf5560c4606230d2f60c8b703731c5bbf0ebb
Knight, Eddie <iv.eddieknight@gmail.com>
2020-06-10 16:12:34 -0400

    Updated config to match TSV data

--- app/associative_tables.js
+++ app/associative_tables.js
@@ -1,45 +1,37 @@
 module.exports = {
-    // spell_descriptors: {
-    //     model_name: "Descriptor",
-    //     joins_to: 'descriptors',
-    //     supports: ['spells'],
-    // },
-    // theme_modifiers: {
-    //     model_name: "Modifier",
-    //     joins_to: 'modifiers',
-    //     supports: ['themes'],
-    // },
-    // class_proficiencies: {
-    //     model_name: "Feat",
-    //     joins_to: 'feats',
-    //     supports: ['classes'],
-    // },
-    // class_features: {
-    //     model_name: "ClassFeature",
-    //     supports: ['classes'],
-    // },
-    // class_special_skills: {
-    //     model_name: "ClassSpecialSkills",
-    //     supports: ['class_features', 'class_special_skills'],
-    // },
-    weapon_category: {
-        model_name: "WeaponCategory",
-        supports: 'weapons',
+    classes: {
+        model_name: 'Class',
+        supports: 'spellcaster_progression',
         relationshipType: 'ManyToOne',
     },
-    // effect_ranges: {
-    //     model_name: "EffectRange",
-    //     supports: ['spells'],
-    //     direct: true,
-    // },
-    // magic_schools:  {
-    //     model_name: "MagicSchool",
-    //     supports: ['spells'],
-    //     direct: true,
-    // },
+    class_proficiencies: {
+        model_name: 'Feat',
+        supports: 'classes',
+        relationshipType: 'ManyToMany',
+    },
+    class_features: {
+        model_name: 'ClassFeature',
+        supports: 'classes',
+        relationshipType: 'OneToMany',
+    },
+    class_special_skills: {
+        model_name: 'ClassSpecialSkills',
+        supports: 'class_features',
+        relationshipType: 'OneToMany',
+    },
     companion_abilities: {
-        model_name: "CompanionAbility",
+        model_name: 'CompanionAbility',
         supports: 'companions',
         relationshipType: 'OneToMany',
+    },
+    effect_ranges: {
+        model_name: 'EffectRange',
+        supports: 'spells',
+        relationshipType: 'ManyToOne',
+    },
+    weapon_category: {
+        model_name: 'WeaponCategory',
+        supports: 'weapons',
+        relationshipType: 'ManyToOne',
     },
 }
--- app/conf.js
+++ app/conf.js
@@ -1,13 +1,7 @@
 module.exports = {
     sentryDsn: 'https://d0fca149b2114d8ea7378217a8fa5eb4@sentry.io/5191093',
-    deployment: 'prod', // Change this to prod when building image! (This process is due for improvements)
+    isProduction: false, // Change this to true when building image! (This process is due for improvements)
     dataType: 'tsv',
     port: 80,
-    debugLevel: 0,
-    stackTrace: true,
-    subDebugLevels: {
-        database: 0,
-        definitions: 0,
-        resolvers: 0,
-    }
+    logLevel: 'verbose',
 }
